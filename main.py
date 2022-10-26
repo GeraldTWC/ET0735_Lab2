b59dddab34177484e26a23c4cc1b8b16537e87f8
fishballmilktea <geraldtwc19.21@ichat.sp.edu.sg>
2022-10-26 15:08:29 +0800

    BMI calculator

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 def calculate_bmi(height, weight):
-    print ("Height is " + str(height))
-    print ("Weight is " + str(weight))
+    print("Height is " + str(height))
+    print("Weight is " + str(weight))
 
     bmi = weight/(height*height)
-    print (str(bmi))
+    print(str(bmi))
 
+    if bmi < 18.5:
+        print("Under weight")
+    elif 18.5 <= bmi <= 25:
+        print("Normal weight")
+    else:
+        print("Over weight")
 
-calculate_bmi(height = 1.73, weight = 57)
+calculate_bmi(1.73,57)
+
